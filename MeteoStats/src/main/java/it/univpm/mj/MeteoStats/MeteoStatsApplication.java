64f9a52b02aa0fb64562192fe9832c3e5c173508
joshua-michele <74794423+joshua-michele@users.noreply.github.com>
2020-12-03 17:20:55 +0100

    Add files via upload

---
+++ MeteoStats/src/main/java/it/univpm/mj/MeteoStats/MeteoStatsApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.mj.MeteoStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoStatsApplication.class, args);
+	}
+
+}
---
+++ MeteoStats/src/test/java/it/univpm/mj/MeteoStats/MeteoStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.mj.MeteoStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
